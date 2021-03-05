53ab3acdaa9248d8bb1979494c6fba27e40ea414
Quentin Roy-Foster <quentinrf@gmail.com>
2021-03-05 14:47:49 -0500

    got eye transforms working in our app

--- Anti_Peep/Anti_Peep/AppDelegate.swift
+++ Anti_Peep/Anti_Peep/AppDelegate.swift
@@ -8,31 +8,54 @@
 
 import UIKit
 import AVKit
+import ARKit
+
+//@UIApplicationMain
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//    
+//
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Override point for customization after application launch.
+//        
+//        return true
+//    }
+//
+//    // MARK: UISceneSession Lifecycle
+//
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    }
+//
+//
+//}
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
     
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        if !ARFaceTrackingConfiguration.isSupported {
+            /*
+             Shipping apps cannot require a face-tracking-compatible device, and thus must
+             offer face tracking AR as a secondary feature. In a shipping app, use the
+             `isSupported` property to determine whether to offer face tracking AR features.
+             This sample code has no features other than a demo of ARKit face tracking, so
+             it replaces the AR view (the initial storyboard in the view controller) with
+             an alternate view controller containing a static error message.
+             */
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "unsupportedDeviceMessage")
+        }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
---
+++ Anti_Peep/Anti_Peep/TransformVisualization.swift
@@ -0,0 +1,66 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Displays coordinate axes visualizing the tracked face pose (and eyes in iOS 12).
+*/
+
+import ARKit
+import SceneKit
+
+class TransformVisualization: NSObject, VirtualContentController {
+    
+    var contentNode: SCNNode?
+    
+    // Load multiple copies of the axis origin visualization for the transforms this class visualizes.
+    lazy var rightEyeNode = SCNReferenceNode(named: "coordinateOrigin")
+    lazy var leftEyeNode = SCNReferenceNode(named: "coordinateOrigin")
+    
+    /// - Tag: ARNodeTracking
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        // This class adds AR content only for face anchors.
+        guard anchor is ARFaceAnchor else { return nil }
+        
+        // Load an asset from the app bundle to provide visual content for the anchor.
+        contentNode = SCNReferenceNode(named: "coordinateOrigin")
+        
+        guard #available(iOS 12.0, *), let faceAnchor = anchor as? ARFaceAnchor
+            else { return nil}
+        
+        rightEyeNode.simdTransform = faceAnchor.rightEyeTransform
+        leftEyeNode.simdTransform = faceAnchor.leftEyeTransform
+        
+        guard #available(iOS 12.0, *), let anchorNode = contentNode else { return nil }
+        
+        // Scale down the coordinate axis visualizations for eyes.
+        rightEyeNode.simdPivot = simd_float4x4(diagonal: SIMD4(3,3,3,1))
+        leftEyeNode.simdPivot = simd_float4x4(diagonal: SIMD4(3,3,3,1))
+        
+        anchorNode.addChildNode(rightEyeNode)
+        anchorNode.addChildNode(leftEyeNode)
+        
+        
+        // Provide the node to ARKit for keeping in sync with the face anchor.
+        return contentNode
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor){
+        guard #available(iOS 12.0, *), let faceAnchor = anchor as? ARFaceAnchor
+            else { return }
+        
+        rightEyeNode.simdTransform = faceAnchor.rightEyeTransform
+        leftEyeNode.simdTransform = faceAnchor.leftEyeTransform
+    }
+    
+    func addEyeTransformNodes() {
+        guard #available(iOS 12.0, *), let anchorNode = contentNode else { return }
+        
+        // Scale down the coordinate axis visualizations for eyes.
+        rightEyeNode.simdPivot = simd_float4x4(diagonal: SIMD4(3,3,3,1))
+        leftEyeNode.simdPivot = simd_float4x4(diagonal: SIMD4(3,3,3,1))
+        
+        anchorNode.addChildNode(rightEyeNode)
+        anchorNode.addChildNode(leftEyeNode)
+    }
+
+}
